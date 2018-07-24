067324015ffd8f79f2951614faa451f3dc91f440
codisfy <codisfy@gmail.com>
2018-07-24 08:31:44 -0400

    styling to win/lose

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -14,8 +14,8 @@ class Game extends Component {
         let { gameState, cards } = this.props.game;
         return (
             <div>
-                {(constants.GAME_WON === gameState && <h1>You Won </h1>) ||
-                    (constants.GAME_LOST === gameState && <h1>You Lose </h1>) ||
+                {(constants.GAME_WON === gameState && <h1 className="end-game win">You Won </h1>) ||
+                    (constants.GAME_LOST === gameState && <h1 className="end-game lose">You Lose </h1>) ||
                     ((constants.NEW_GAME === gameState || constants.GAME_STARTED === gameState) && (
                         cards.map((card) => {
                             return <Card key={card.id} {...card} game={this.props.game} showing={card.cardState !== constants.CARD_HIDDEN} handleCardClicked={this.props.handleCardClicked} />
